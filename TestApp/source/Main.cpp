1f5107e71b2e95797b353f82707819121c82ce7a
J-D-K <gerplefart@gmail.com>
2025-08-05 13:35:59 -0400

    Add sdl::Surface, functions. Refactor.

--- TestApp/source/Main.cpp
+++ TestApp/source/Main.cpp
@@ -1,38 +1,47 @@
 #include "sdl.hpp"
+
+#include <fstream>
+#include <string_view>
 #include <switch.h>
 
 static constexpr sdl::Color BLACK = {0x000000FF};
 static constexpr sdl::Color WHITE = {0xFFFFFFFF};
 
+static constexpr std::string_view TEST_TEXT   = "Test Text here.";
+static constexpr std::string_view EXIT_STRING = "Press \uE0EF to exit.";
+
+static void log(std::string_view text)
+{
+    std::ofstream logFile{"sdmc:/sdllib.log", std::ios::app};
+    if (!logFile.is_open()) { return; }
+    logFile << text << std::endl;
+}
+
 int main()
 {
-    if (!sdl::initialize("sdl test app", 1280, 720))
-    {
-        return -1;
-    }
+    if (!sdl::initialize("sdl test app", 1280, 720)) { return -1; }
 
-    if (!sdl::text::initialize())
-    {
-        return -2;
-    }
+    if (!sdl::text::initialize()) { return -2; }
 
     PadState gamePad;
     padConfigureInput(1, HidNpadStyleSet_NpadStandard);
     padInitializeDefault(&gamePad);
 
-    size_t testWidth = sdl::text::get_width(18, "Test text");
+    size_t testWidth              = sdl::text::get_width(18, TEST_TEXT);
+    const std::string widthString = std::to_string(testWidth);
 
     while (appletMainLoop())
     {
         padUpdate(&gamePad);
 
-        if (padGetButtonsDown(&gamePad) & HidNpadButton_Plus)
-        {
-            break;
-        }
+        if (padGetButtonsDown(&gamePad) & HidNpadButton_Plus) { break; }
 
         sdl::frame_begin(WHITE);
-        sdl::text::render(NULL, 32, 32, 18, sdl::text::NO_TEXT_WRAP, BLACK, "Text here.\nPress \uE0EF to exit.");
+
+        int y = 32;
+        sdl::text::render(sdl::Texture::Null, 32, y, 18, sdl::text::NO_WRAP, BLACK, TEST_TEXT);
+        sdl::text::render(sdl::Texture::Null, 32, (y += 32), 18, sdl::text::NO_WRAP, BLACK, EXIT_STRING);
+        sdl::text::render(sdl::Texture::Null, 32, (y += 32), 18, sdl::text::NO_WRAP, BLACK, widthString);
         sdl::frame_end();
     }
     sdl::text::exit();
